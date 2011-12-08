8f458a440ea9d68c299e238b338484f6475b5b36
anateus <none@none>
2011-12-07 22:17:00 -0800

    added a design comment to models

--- MovieSneaker/sneaker/models.py
+++ MovieSneaker/sneaker/models.py
@@ -8,3 +8,24 @@ class ZipCode(models.Model):
 class ZipCodeForm(ModelForm):
     class Meta:
         model = ZipCode 
+
+design_comment = """
+- Movie
+  - name :: VARCHAR(256) // Movie Name (Year) guarantees unique, we'll figure something out if there's an exception
+  - runtime (seconds) :: INTEGER
+  - description :: VARCHAR(1024)
+  - notes :: VARCHAR(512)
+
+- Showing
+  - movie :: ForeignKey(Movie)
+  - venue :: ForeignKey(Venue)
+  - start :: DATETIME
+  - end :: DATETIME
+
+- Venue
+  - id
+  - name :: VARCHAR(256)
+  - address :: VARCHAR(512)
+  - zipcode :: CHAR(5)
+  - description :: VARCHAR(1024)
+"""
