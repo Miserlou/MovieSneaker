ddd59297cc5b906d1651720e1592003ccd0713e7
anateus <katsevm@gmail.com>
2011-12-09 05:07:35 -0800

    showtime parsing updated

--- MovieSneaker/sneaker/showtimesparsing.py
+++ MovieSneaker/sneaker/showtimesparsing.py
@@ -1,3 +1,6 @@
+import lxml.html as html
+import datetime
+
 class ShowtimeParser:
 	pass
 
@@ -5,14 +8,17 @@ class FlixsterParser(ShowtimeParser):
 	# The date is in the format: YYYYMMDD, zipcode is: NNNNN
 	BASE_URL = "http://igoogle.flixster.com/igoogle/showtimes?movie=all&date=%(date)s&postal=%(zipcode)s&submit=Go"
 
-	def __init__(self,date,zipcode):
+	def __init__(self,zipcode=None,date=None):
 		"""
 		:param date: Datetime object representing our target date
 		:param zipcode: The zipcode as a string
 
 		:return:
 		"""
-		self._get_base(self,date=date.strftime("%Y%m%d"),zipcode=zipcode)
-
+		if zipcode:
+			if not date:
+				date = datetime.datetime.today()
+				self._get_base(date=date.strftime("%Y%m%d"),zipcode=zipcode)
+	
 	def _get_base(self,**kwargs):
-		urllib2.urlopen(BASE_URL%kwargs)
+		self.base = html.parse(BASE_URL%kwargs).getroot())
