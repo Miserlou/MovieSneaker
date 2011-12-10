ce021d4afadadd1536356f995a41704a012554a1
anateus <katsevm@gmail.com>
2011-12-10 02:32:38 -0800

    working parser

--- MovieSneaker/sneaker/showtimesparsing.py
+++ MovieSneaker/sneaker/showtimesparsing.py
@@ -1,5 +1,6 @@
 import lxml.html as html
 import datetime
+import re
 
 # TODO: Refactor this shitshow. Just use a much more straightforward interface:
 # Basically, I need to define an intermediate data model, perhaps json
@@ -8,20 +9,8 @@ import datetime
 
 class ShowtimeParser:
 	base = None
-	venues = None
-	movies = None
-	showtimes = None
 	zipcode = None
 	date = None
-
-	def get_venues(self,**kwargs):
-		# if any parameters are different from the class variables, reinitialize
-		if [True for k,v in [(k,self.__dict__[k]) for k in kwargs.keys() if self.__dict__.has_key(k)] if v!=d1[k]]
-			self.__init__(**kwargs)
-		if not self.venues:
-			# extract venues
-			self._extract_venues()
-		return self.venues
 		
 # TODO: fake the useragent
 
@@ -44,9 +33,11 @@ class FlixsterParser(ShowtimeParser):
 			self.zipcode = zipcode
 			self.date = date
 			self._get_base(date=date.strftime("%Y%m%d"),zipcode=zipcode)
+			self._parse()
+			
 	
 	def _get_base(self,**kwargs):
-		self.base = html.parse(BASE_URL%kwargs).getroot())
+		self.base = html.parse(self.BASE_URL%kwargs).getroot()
 
 	def _parse(self):
 		theatres = []
@@ -55,15 +46,40 @@ class FlixsterParser(ShowtimeParser):
 			address = theater.cssselect('h2 span')[0].text.strip("\n\t -")
 			movies = []
 			for movie in theater.cssselect('.showtime'):
-				title = movie.cssselect('h3 a').attrib['title']
-				raw_rating,raw_duration = movie.cssselect('h3 span').text.strip("\n\t -").split('-')
-				showings = []
-				for raw_time in movie.cssselect('h3').tail.split()
-					if raw_time.endswith(am):
+				title_line = movie.cssselect('h3')[0]
+				if len(title_line.getchildren())>1:
+					title = title_line.cssselect('a')[0].attrib['title']
+					sp = title_line.cssselect('span')[0].text.strip("\n\t -").rsplit('-',1)
+					raw_rating = sp[0]
+					if len(sp)>1:
+						raw_duration = sp[1]
+						split_duration = raw_duration.strip().split()
+						parsed_duration = 0
+						while split_duration:
+							num = split_duration.pop(0)
+							kind = split_duration.pop(0)
+							parsed_duration += 60*int(num)if 'hr' in kind.lower() else int(num)
+						duration = datetime.timedelta(minutes=parsed_duration)
+					else:
+						# if no duration specified we'll assume 3 hours
+						# probably double feature
+						duration = datetime.timedelta(minutes=180)
+				else: # we have nothing but a non-linked title
+					title = title_line.text.strip()
+					duration = datetime.timedelta(minutes=90)
+				showtimes = []
+				for raw_time in movie.cssselect('h3')[0].tail.split():
+					if not raw_time.endswith("am"):
 						raw_time += "pm"
 					# combine the hour with the date of this parse
-					start = datetime.datetime.combine(self.date,datetime.datetime.strptime(raw_time,"%I:%M%p")
+					start = datetime.datetime.combine(self.date,datetime.datetime.strptime(raw_time,"%I:%M%p").time())
 					end = start + duration
-				movies.append({'name':title,'showtimes':showings})
+					showtimes.append({"start":start,"end":end})
+				movies.append({'name':title,'showtimes':showtimes})
 			theatres.append({'name':name,'address':address,'movies':movies})
+		self.theatres = theatres
 		return theatres
+
+if __name__=="__main__":
+	fp = FlixsterParser("94043")
+	
